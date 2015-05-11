a071450f3e2c63123f60ffd95eb78ce249716361
Shayne <shaynetremblay@hotmail.com>
2015-05-10 21:53:42 -0400

    couple PE's

--- problem12.r
+++ problem12.r
@@ -2,22 +2,22 @@ trinum = 0
 x = 1
 divisors = 0
 
-while divisors < 500
-	trinum = 0
-	for i in 0..x
-		trinum += i
-	end
+while divisors < 700
+	trinum += x
 	#puts trinum
 	check = 1
-	divisors = 0
-	while check <= trinum
+	divisors = 1
+	while check <= (trinum**(0.5))
 		if trinum % check == 0
-			divisors += 1
+			divisors += 2
+		end
+		if ((trinum**(0.5)).to_i * (trinum**(0.5)).to_i) == trinum
 		end
 		check += 1
 	end
-	puts "Divisors = #{divisors}"
+	puts "x = #{x}"
 	puts "trinum = #{trinum}"
+	puts "Divisors = #{divisors}"
 	x += 1
 
 
