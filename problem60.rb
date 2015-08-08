0db046f5d30f98568eecb1b8a73742162c3e3311
Shayne Tremblay <shaynetremblay@hotmail.com>
2015-08-08 14:21:54 -0400

    eh

--- problem60.rb
+++ problem60.rb
@@ -5,11 +5,39 @@ def concateCheck(prime1, prime2)
 	return false
 end
 
-def problem60()
-	primeArray = Array.new
-	Prime.each(10000) {|prime| primeArray.push(prime)}
+def initial_set_create()
 	concateArray = Array.new
-	for prime in 0..primeArray.count-5
-		
-
+	Prime.each(10000) do |prime1|
+		primeArray = Array.new
+		primeArray.push(prime1)
+		Prime.each(10000) do |prime2|
+			if concateCheck(prime1,prime2) and prime1 != prime2
+				primeArray.push(prime2)
+			end
+		end
+		if primeArray.count > 6
+			concateArray.push(primeArray)
+		end
+	end
+	return concateArray
 end
+
+def problem60()
+	primeSet = initial_set_create
+	finalArray = Array.new
+	for prime1 in primeSet
+		tempArray = Array.new
+		tempArray.push(prime1)
+		for prime2 in primeSet
+			if concateCheck(prime1,prime2) and prime1 != prime2
+				tempArray.push(prime2)
+			end
+		end
+		if tempArray.count > 6
+			finalArray.push(tempArray)
+		end
+	end
+	return finalArray.flatten.uniq
+end
+
+print problem60
