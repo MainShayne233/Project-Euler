caa7c221933e59ed473998dc421baa480d10dc73
Main0Shayne <shaynetremblay@hotmail.com>
2015-12-29 00:55:25 -0500

    optimizied problem60 to run in less than 60 seconds

--- problem60.rb
+++ problem60.rb
@@ -1,23 +1,22 @@
 require 'prime'
 
-
+start = Time.now
 def con_both_ways(x, y)
   (("#{x}" + "#{y}").to_i).prime? and (("#{y}" + "#{x}").to_i).prime?
 end
-
 
 primes = Prime.each(10000).to_a
 
 candidates = Hash.new()
 
-for p in primes
-  cand_primes = Array.new()
-  for q in primes
-    if con_both_ways(p,q) and p != q
-      cand_primes.push(q)
-    end
-  end
-  candidates[p] = cand_primes
+for p in 0..primes.length-1
+	cand_primes = Array.new()
+	for q in p..primes.length-1
+		if con_both_ways(primes[p],primes[q])
+			cand_primes.push(primes[q])
+		end
+	end
+	candidates[primes[p]] = cand_primes
 end
 
 for p in primes
@@ -32,7 +31,7 @@ for p in primes
 						for pos3 in candidates[pos2]
 							next if pos3 < p or pos3 < cand or pos3 < pos1 or pos3 < pos2
 							if candidates[p].include? pos3 and candidates[cand].include? pos3 and candidates[pos1].include? pos3
-								puts "#{p} #{cand} #{pos1} #{pos2} #{pos3}"
+								puts [p,cand,pos1,pos2,pos3].reduce(:+)
 							end
 						end
 					end
